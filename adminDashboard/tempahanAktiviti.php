15843a7356f3a9951bf91b7143a0a142886d2839
niknadia92 <ndiantsya92@gmail.com>
2024-12-11 14:46:18 +0800

    Update tempahanAktiviti.php

--- adminDashboard/tempahanAktiviti.php
+++ adminDashboard/tempahanAktiviti.php
@@ -96,8 +96,8 @@
 												<tr>
 													<th style="width: 20px;">
 														<div class="form-check">
-															<input type="checkbox" class="form-check-input" id="customCheck<?php echo htmlspecialchars($id_aktiviti); ?>">
-															<label class="form-check-label" for="customCheck<?php echo htmlspecialchars($id_aktiviti); ?>">&nbsp;</label>
+															<input type="checkbox" class="form-check-input" id="customCheck1">
+                                                            <label class="form-check-label" for="customCheck1">&nbsp;</label>
 														</div>
 													</th>
 													<th class="all">Nombor Tempahan</th>
